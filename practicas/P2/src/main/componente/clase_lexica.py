9bdc9a227b3b5ed22d0e003c69ca137b3db6a87e
MarkTwin25 <jesusmartin-25@ciencias.unam.mx>
2025-03-19 20:03:25 -0600

    Practica terminada

--- practicas/P2/src/main/componente/clase_lexica.py
+++ practicas/P2/src/main/componente/clase_lexica.py
@@ -1,9 +1,30 @@
 from enum import Enum
 
 class ClaseLexica(Enum):
-    PALABRA = 1 
-    NUMERO = 2
-    PARIZQ = 3
-    PARDER = 4
-    ESPACIO = 5
-    # TODO: agregar las clases léxicas restantes
+    """
+    Enumeración que representa las clases léxicas de los componentes léxicos (tokens).
+    
+    Atributos:
+    - INT (int): palabra reservada 'int'.
+    - FLOAT (int): palabra reservada 'float'.
+    - IF (int): palabra reservada 'if'.
+    - ELSE (int): palabra reservada 'else'.
+    - WHILE (int): palabra reservada 'while'.
+    - NUMERO (int): número.
+    - ID (int): identificador.
+    - PYC (int): punto y coma.
+    - COMA (int): coma.
+    - LPAR (int): paréntesis izquierdo.
+    - RPAR (int): paréntesis derecho.
+    """
+    INT = 1
+    FLOAT = 2
+    IF = 3
+    ELSE = 4
+    WHILE = 5
+    NUMERO = 6
+    ID = 7
+    PYC = 8       # Punto y coma
+    COMA = 9      # Coma
+    LPAR = 10     # Paréntesis izquierdo
+    RPAR = 11     # Paréntesis derecho
